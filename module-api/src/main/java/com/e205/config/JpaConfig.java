ef5969b434eda18b2c879e7576ec595913e874c9
권대호 <trewq231@naver.com>
2024-11-07 09:05:02 +0900

    [BE] chore#92: yml 설정 추가
    :tada: :tada: :tada:
    권대호 <trewq231@naver.com>님이 Closes #92을 완료했습니다. 
    
    커밋 목록
    * chore: yml 설정 추가
    
    * refactor: N + 1 기능 리팩토링
    
    승인자
    Approved-by: 이현수 <znight1020@naver.com>

--- module-api/src/main/java/com/e205/config/JpaConfig.java
+++ module-api/src/main/java/com/e205/config/JpaConfig.java
@@ -2,10 +2,13 @@ package com.e205.config;
 
 import com.e205.log.LogInterceptor;
 import com.e205.log.TransactionSynchronizationRegistryImpl;
+import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -13,6 +16,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@RequiredArgsConstructor
 @Slf4j
 @Configuration
 public class JpaConfig {
@@ -22,14 +26,16 @@ public class JpaConfig {
   private static final String HIBERNATE_FORMAT_SQL_KEY = "hibernate.format_sql";
   private static final String HIBERNATE_HBM2DDL_AUTO_KEY = "hibernate.hbm2ddl.auto";
 
-  @Value("${hibernate.hbm2ddl.auto:#{'none'}}")
+  @Value("${spring.jpa.hibernate.ddl-auto:#{'none'}}")
   private String ddlValue;
-  @Value("${hibernate.generateddl:#{false}}")
+  @Value("${spring.jpa.generate-ddl:#{false}}")
   private boolean generateDdl;
-  @Value("${hibernate.showsql:#{false}}")
+  @Value("${spring.jpa.show-sql:#{false}}")
   private boolean showSql;
-  @Value("${hibernate.formatsql:#{false}}")
+  @Value("${spring.jpa.properties.hibernate.format_sql:#{false}}")
   private boolean formatSql;
+
+  private final JpaProperties jpaProperty;
 
   @Bean
   public LogInterceptor logInterceptor() {
@@ -48,10 +54,11 @@ public class JpaConfig {
     emf.setDataSource(dataSource);
     emf.setPackagesToScan(ENTITY_PACKAGE_TO_SCAN);
 
-    Map<String, Object> jpaProperties = emf.getJpaPropertyMap();
+    Map<String, Object> jpaProperties = new HashMap<>(jpaProperty.getProperties());
     jpaProperties.put(HIBERNATE_SESSION_FACTORY_INTERCEPTOR, logInterceptor());
     jpaProperties.put(HIBERNATE_FORMAT_SQL_KEY, formatSql);
     jpaProperties.put(HIBERNATE_HBM2DDL_AUTO_KEY, ddlValue);
+    emf.setJpaPropertyMap(jpaProperties);
 
     return emf;
   }
