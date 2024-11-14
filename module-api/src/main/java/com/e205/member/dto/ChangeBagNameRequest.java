da303245c70892758a70da4ff17002ce528fc03d
홍성우 <sungwoo166@gmail.com>
2024-11-14 17:53:07 +0900

    [BE] refactor#196: 가방요청 Valid 변경
    :tada: :tada: :tada:
    홍성우 <sungwoo166@gmail.com>님이 을 완료했습니다. 
    
    커밋 목록
    * refactor: 이름 변경 Valid 변경
    
    승인자
    Approved-by: 이현수 <znight1020@naver.com>

--- module-api/src/main/java/com/e205/member/dto/ChangeBagNameRequest.java
+++ module-api/src/main/java/com/e205/member/dto/ChangeBagNameRequest.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ChangeBagNameRequest(
-    @NotNull(message = "bagId는 null일 수 없습니다.")
     Integer bagId,
     @NotBlank(message = "이름은 공백일 수 없습니다.")
     @Size(min = 1, max = 20, message = "이름은 {min} 부터 {max}자 사이여야 합니다.")
