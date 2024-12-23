20a3292df1196e83f11afdd34b31d900b8d518cc
kwondh5217 <trewq231@naver.com>
2024-12-24 04:04:06 +0900

    feat: 패키지 간소화

--- module-common/src/main/java/com/e205/base/member/command/member/command/VerifyEmailToken.java
+++
@@ -1,7 +0,0 @@
-package com.e205.base.member.command.member.command;
-
-public record VerifyEmailToken(
-    String token
-) {
-
-}
--- module-common/src/main/java/com/e205/base/route/payload/RoutesPayload.java
+++ module-common/src/main/java/com/e205/base/route/payload/RoutesPayload.java
@@ -3,7 +3,6 @@ package com.e205.base.route.payload;
 import com.e205.base.route.dto.RoutePart;
 import java.util.List;
 import lombok.Builder;
-import org.locationtech.jts.geom.LineString;
 
 @Builder
 public record RoutesPayload(
