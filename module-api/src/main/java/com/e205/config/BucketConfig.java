54ab40ad25e2d4f2ddc5e8cbc95101fdc6661d16
Daeho Kwon <trewq231@naver.com>
2024-12-26 15:54:43 +0900

    feat: Http Method 별 버킷 전략 설정

--- module-api/src/main/java/com/e205/config/BucketConfig.java
+++ module-api/src/main/java/com/e205/config/BucketConfig.java
@@ -1,40 +1,24 @@
 package com.e205.config;
 
+import static java.time.Duration.ofMillis;
 import static java.time.Duration.ofSeconds;
 
 import io.github.bucket4j.BucketConfiguration;
-import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
-import io.github.bucket4j.redis.lettuce.Bucket4jLettuce;
-import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.codec.ByteArrayCodec;
-import io.lettuce.core.codec.RedisCodec;
-import io.lettuce.core.codec.StringCodec;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-@Configuration
+@Component
 public class BucketConfig {
 
-  private final RedisClient redisClient;
+  public BucketConfiguration createBucketConfiguration(HttpMethod method) {
+    if(method == HttpMethod.POST) {
+      return BucketConfiguration.builder()
+          .addLimit(limit -> limit.capacity(1).refillGreedy(1, ofMillis(500)))
+          .build();
+    }
 
-  @Bean
-  public LettuceBasedProxyManager lettuceBasedProxyManager() {
-    StatefulRedisConnection<String, byte[]> connect = redisClient.connect(
-        RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
-    return Bucket4jLettuce.casBasedBuilder(connect)
-        .expirationAfterWrite(
-            ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(ofSeconds(10)))
-        .build();
-  }
-
-  @Bean
-  public BucketConfiguration bucketConfiguration() {
     return BucketConfiguration.builder()
-        .addLimit(limit -> limit.capacity(1).refillGreedy(1, ofSeconds(1)))
+        .addLimit(limit -> limit.capacity(10).refillGreedy(10, ofSeconds(1)))
         .build();
   }
 }
